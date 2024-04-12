59006fa9ba0b5346dcb902c2667a4f9d63e6b5d0
omar-dulaimi <o.m.dulaimi@gmail.com>
2024-04-12 22:49:03 +0300

    refactor exporting

--- src/commands/save-current-breakpoints.cmd.ts
+++ src/commands/save-current-breakpoints.cmd.ts
@@ -1,12 +1,17 @@
-import * as vscode from "vscode";
-import * as path from "path";
 import { writeFile } from "fs/promises";
+import * as path from "path";
+import * as vscode from "vscode";
 import { BreakpointBookmarksProvider } from "../providers/breakpoint-bookmarks.provider";
 
 export const saveCurrentBreakpoints =
   (provider: BreakpointBookmarksProvider) => async () => {
     const workspacePath = vscode.workspace.workspaceFolders?.[0]?.uri
       ?.fsPath as string;
+
+    if (!workspacePath) {
+      vscode.window.showInformationMessage("No workspace opened!");
+      return;
+    }
     const config = vscode.workspace.getConfiguration("breakpointBookmark");
     const saveLocation = config.get("saveLocation") as string;
     await provider.assureSaveDirectoryExist(saveLocation, workspacePath);
@@ -14,16 +19,25 @@ export const saveCurrentBreakpoints =
       title: "Enter file name without extension",
       placeHolder: "test express bug",
     });
-    const currentBreakpoints = vscode.debug.breakpoints.map(
-      (bp: vscode.Breakpoint) => ({
-        location: (bp as any).location.uri.path,
-        range: (bp as any).location.range,
+
+    const currentBreakpoints = (
+      vscode.debug.breakpoints as vscode.SourceBreakpoint[]
+    ).map((bp: vscode.SourceBreakpoint) => {
+      const location: string = bp.location.uri.path;
+      const range: vscode.Range = bp.location.range.with({
+        start: bp.location.range.start.translate(1),
+        end: bp.location.range.end.translate(1),
+      });
+
+      return {
+        location,
+        range,
         enabled: bp.enabled,
         condition: bp.condition,
         hitCondition: bp.hitCondition,
         logMessage: bp.logMessage,
-      })
-    );
+      };
+    });
 
     const filePath = saveLocation
       ? `${saveLocation}/${fileName}.json`
@@ -32,5 +46,6 @@ export const saveCurrentBreakpoints =
     await writeFile(filePath, JSON.stringify(currentBreakpoints), {
       encoding: "utf-8",
     });
+
     provider.refresh();
   };
